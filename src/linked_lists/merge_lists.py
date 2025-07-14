129a2f5c33793216d3089fc9fe09db82679f8b96
kaushik <kaushikam@gmail.com>
2025-07-14 10:27:11 +0530

    created recursive merge_lists

--- src/linked_lists/merge_lists.py
+++ src/linked_lists/merge_lists.py
@@ -27,5 +27,25 @@ from .node import Node
 #     return dummy_head.next
 
 
-# Space complexity: O(1)
+def merge_lists(head_1: Optional[Node], head_2: Optional[Node]) -> Optional[Node]:
+    if head_1 is None and head_2 is None:
+        return None
+
+    if head_1 is None:
+        return head_2
+
+    if head_2 is None:
+        return head_1
+
+    if head_1.val > head_2.val:
+        next = head_2.next
+        head_2.next = merge_lists(head_1, next)
+        return head_2
+    else:
+        next = head_1.next
+        head_1.next = merge_lists(next, head_2)
+        return head_1
+
+
+# Space complexity: O(1). In the case of recursion, O(min(m,n))
 # Time complexity: O(min(m,n))
