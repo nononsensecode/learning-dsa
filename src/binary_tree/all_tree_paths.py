b1688aff0d701fc2d368cf98badd0e6eb6de104c
kaushik <kaushikam@gmail.com>
2025-07-22 18:39:14 +0530

    added complexities for all_tree_paths

--- src/binary_tree/all_tree_paths.py
+++ src/binary_tree/all_tree_paths.py
@@ -23,5 +23,55 @@ def all_tree_paths(root: Optional[Node]) -> List[List[Any]]:
 
     return [*left_paths, *right_paths]
 
-# Time complexity: O(n)
-# Space complexity: O(n)
+#      a
+#    /   \
+#   b     c
+#  / \    / \
+# d   e  f   g
+"""
+In the above diagram, you can see that the number of nodes in this balanced tree:
+
+Nodes = 7
+Leaves = 4
+Balance (Other nodes)  = 3
+"""
+#          a
+#        /   \
+#       b     c
+#      / \    / \
+#     d   e  f   g
+#   / \  / \ / \ / \
+#  h  i j  k l m n o
+"""
+In the above diagram:
+Nodes = 15
+Leaves = 8
+Other nodes = 7
+
+So there is a relationship between "Other nodes" and "leaves", and that is:
+Other Nodes = Leaves - 1
+Total number of nodes = Other Nodes + Leaves = Leaves + Leaves - 1 = 2 * (Leaves) - 1
+As 1 is a constant, we can ignore it.
+
+So total nodes = 2 * (Leaves), or in another way:
+
+Number of leaves (l) = Number of nodes/2 or n/2
+
+There is a relationship we can derive from the height(h) and number of nodes(n) also:
+n = 2^h - 1
+
+we can ignore the constant 1, so:
+n = 2^h
+
+We can write it in logarithmic way as follows:
+log(n) = h
+
+So we have the following:
+nodes = n
+height = log(n)
+leaves = n/2
+
+time complexity = O(number of leaves * height) = O(n/2 * log(n)) = O(n * log(n))
+space complexity = O(n * log(n))
+"""
+
