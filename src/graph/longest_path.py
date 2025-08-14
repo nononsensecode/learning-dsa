12dc97cb90881ef8c2758513bc5e187b715e4beb
kaushik <kaushikam@gmail.com>
2025-08-14 10:27:42 +0530

    Updated variable names and extracted a function

--- src/graph/longest_path.py
+++ src/graph/longest_path.py
@@ -2,27 +2,31 @@ from typing import Dict, List
 
 
 def longest_path(graph: Dict[str, List[str]]) -> int:
-    distances = {}
+    distances: Dict[str, int] = _find_end_points_with_distance(graph)
     for node in graph:
-        if len(graph[node]) == 0:
-            distances[node] = 0
-    
-    for node in graph:
-        traverse_distances(graph, node, distances)
+        _traverse(graph, node, distances)
 
     return max(distances.values())
 
 
-def traverse_distances(graph: Dict[str, List[str]], node: str, distances: Dict[str, int]) -> int:
+def _find_end_points_with_distance(graph: Dict[str, List[str]]) -> Dict[str, int]:
+    ending_points: Dict[str, int] = {}
+    for node in graph:
+        if len(graph[node]) == 0:
+            ending_points[node] = 0
+
+    return ending_points
+
+
+def _traverse(graph: Dict[str, List[str]], node: str, distances: Dict[str, int]) -> int:
     if node in distances:
         return distances[node]
-    
-    largest = 0
+
+    largest_distance = 0
     for neighbour in graph[node]:
-        attempt = traverse_distances(graph, neighbour, distances)
-        if attempt > largest:
-            largest = attempt
+        distance = _traverse(graph, neighbour, distances)
+        if distance > largest_distance:
+            largest_distance = distance
 
-    distances[node] = 1 + largest
+    distances[node] = 1 + largest_distance
     return distances[node]
-
